7a7074749ea743d676eaa3b6d60c29e1410e5f66
Kevin Isikayadir <iskrida42@gmail.com>
2022-12-05 23:03:20 +0100

    ex00 done

--- ex00/src/megaphone.cpp
+++ ex00/src/megaphone.cpp
@@ -5,19 +5,15 @@ int main(int argc, char const *argv[])
     std::string str_toconvert = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     std::string str_uppercased;
 
-    if (argc == 2)
-        str_toconvert = argv[1];
-    else if (argc != 1)
-        return 1;
+    if (argc > 1) {
+        str_toconvert = *++argv;
+        while (*++argv)
+            str_toconvert.append(*argv);
+    }
 
-    std::cout << "Length -> " << str_toconvert.length() << std::endl;
-    for (size_t i = 0; i < str_toconvert.length(); i++)
-        std::cout << "caractere : " << str_toconvert[i] << std::endl;
-
-    std::cout << std::endl;
     for (std::string::iterator it = str_toconvert.begin(); it != str_toconvert.end(); it++)
-        std::cout << "kara : " << *it << std::endl;
+        str_uppercased.push_back(std::toupper(*it));
 
-    std::cout << str_toconvert << std::endl;
+    std::cout << str_uppercased << std::endl;
     return 0;
 }
