00168588cee2dc8e292764882ae97f4f7cd32621
Kevin ISIKAYADIR <kisikaya@e1r11p20.clusters.42paris.fr>
2023-01-16 21:29:22 +0100

    ex01 - c06 done sur le reinterpret cast avec Data et uintptr_t

---
+++ C06/ex01/include/Data.hpp
@@ -0,0 +1,13 @@
+#ifndef DATA_HPP
+# define DATA_HPP
+# pragma once
+
+struct Data {
+public:
+    int x;
+    int y;
+};
+
+
+
+#endif
---
+++ C06/ex01/src/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "Data.hpp"
+
+typedef unsigned long uintptr_t;
+
+uintptr_t serialize(Data *ptr) {
+    return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data * deserialize(uintptr_t raw) {
+    return reinterpret_cast<Data*>(raw);
+}
+
+int main(void) {
+
+    Data data;
+
+    data.x = 42;
+    data.y = 66;
+
+    Data * dataptr = &data;
+
+    uintptr_t serialized = serialize(dataptr);
+    Data * deserialized = deserialize(serialized);
+
+    data.x = 43;
+    data.y = 67;
+
+    std::cout << "data.x = " << data.x <<  std::endl;    
+    std::cout << "data.y = " << data.y <<  std::endl; 
+    std::cout << std::endl;
+
+    std::cout << "Deserialized.x = " << deserialized->x << std::endl;
+    std::cout << "deserialized.y = " << deserialized->y << std::endl; 
+
+    return 0;
+}
